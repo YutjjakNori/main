bc38404406ab52d0155bebab2e06c9aa877bdba7
김정은 <jegarnet23@gmail.com>
2023-04-18 14:06:44 +0900

    Setting: 프로젝트 폴더 생성

---
+++ back-yut/src/main/java/com/ssafy/yut/BackYutApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.yut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackYutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackYutApplication.class, args);
+	}
+
+}
