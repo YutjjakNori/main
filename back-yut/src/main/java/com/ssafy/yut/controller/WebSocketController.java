74e11358599b750ff6ad1237545c2a8ec9ae3ea3
jun <ktf1406@naver.com>
2023-04-24 16:06:12 +0900

    Feat : Controller 생성

---
+++ back-yut/src/main/java/com/ssafy/yut/controller/WebSocketController.java
@@ -0,0 +1,14 @@
+package com.ssafy.yut.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class WebSocketController {
+
+
+}
