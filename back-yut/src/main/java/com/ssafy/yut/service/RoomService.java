2b0b17a2f6c3637ca6a4b9e099303cb7d5b7f873
김정은 <jegarnet23@gmail.com>
2023-04-25 15:28:14 +0900

    Modify : JavaDoc 추가

--- back-yut/src/main/java/com/ssafy/yut/service/RoomService.java
+++ back-yut/src/main/java/com/ssafy/yut/service/RoomService.java
@@ -6,6 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+/**
+ * 대기방 관련 Service
+ *
+ * @author 김정은
+ */
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -13,6 +19,11 @@ public class RoomService {
     private final int RANDOM_LEN = 5;
     private final boolean USE_LETTERS = true, USE_NUMBERS = true;
 
+    /**
+     * 방 생성할 때 RoomCode 생성
+     *
+     * @return RoomCode
+     */
     public RoomDto createRoom(){
         String code = RandomStringUtils.random(RANDOM_LEN, USE_LETTERS, USE_NUMBERS);
         return new RoomDto(code);
