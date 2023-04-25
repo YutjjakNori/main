cd050b2bb2c2e2c170820d30f748fd7ca66b265b
김정은 <jegarnet23@gmail.com>
2023-04-25 15:28:36 +0900

    Refac : URI 수정

--- back-yut/src/main/java/com/ssafy/yut/controller/RoomController.java
+++ back-yut/src/main/java/com/ssafy/yut/controller/RoomController.java
@@ -2,8 +2,11 @@ package com.ssafy.yut.controller;
 
 import com.ssafy.yut.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,11 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/room")
+@MessageMapping("/room")
+@Slf4j
 public class RoomController {
 
     private final RoomService roomService;
 
-    @GetMapping("/api/room/made")
+    /**
+     * 방 생성하기
+     *
+     * @return 생성한 RoomCode
+     */
+    @GetMapping("/made")
     public ResponseEntity<?> createRoom(){
         return ResponseEntity.ok(roomService.createRoom());
     }
