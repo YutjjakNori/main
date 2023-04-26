44b28e1edcb11886aa6777968d421cd220233a9e
jun <ktf1406@naver.com>
2023-04-26 17:47:12 +0900

    Modify : 대기방 입장 기능 추가

--- back-yut/src/main/java/com/ssafy/yut/controller/RoomController.java
+++ back-yut/src/main/java/com/ssafy/yut/controller/RoomController.java
@@ -1,11 +1,14 @@
 package com.ssafy.yut.controller;
 
+import com.ssafy.yut.dto.RoomDto;
 import com.ssafy.yut.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +35,15 @@ public class RoomController {
     @GetMapping("/made")
     public ResponseEntity<?> createRoom(){
         return ResponseEntity.ok(roomService.createRoom());
+    }
+
+    @PostMapping("/entry")
+    public ResponseEntity<?> enterRoom(@RequestBody RoomDto.RoomCode roomDto) {
+        return ResponseEntity.ok(roomService.enterRoom(roomDto));
+    }
+
+    @MessageMapping("/enter")
+    public void enterRoom(@RequestBody RoomDto.RequestEnter enterDto) {
+        roomService.enterRoom(enterDto);
     }
 }
