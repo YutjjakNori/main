45effd5fbebdf028cb42569e3b117687d7acc966
jun <ktf1406@naver.com>
2023-04-26 17:46:44 +0900

    Modify : Room Dto 수정

--- back-yut/src/main/java/com/ssafy/yut/dto/RoomDto.java
+++ back-yut/src/main/java/com/ssafy/yut/dto/RoomDto.java
@@ -6,16 +6,51 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 /**
  * 대기방 관련 DTO
  *
  * @author 김정은
  */
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class RoomDto {
-    private String roomCode;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class User {
+        private String userId;
+        private String nickName;
+    }
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RequestEnter {
+        private String roomCode;
+        private String userId;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class RoomCode {
+        private String roomCode;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ResponseEnter {
+        private List<User> users;
+        private String ready;
+    }
 }
