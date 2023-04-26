1ead75977afed0e11dccf0c74ae207c020c4db5a
jun <ktf1406@naver.com>
2023-04-26 17:47:18 +0900

    Modify : 대기방 입장 기능 추가

--- back-yut/src/main/java/com/ssafy/yut/service/RoomService.java
+++ back-yut/src/main/java/com/ssafy/yut/service/RoomService.java
@@ -4,6 +4,9 @@ import com.ssafy.yut.dto.RoomDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,6 +19,10 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class RoomService {
+
+    private static final String TOPIC = "room", GROUP_ID = "yut";
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final SimpMessagingTemplate template;
     private final int RANDOM_LEN = 5;
     private final boolean USE_LETTERS = true, USE_NUMBERS = true;
 
@@ -24,8 +31,24 @@ public class RoomService {
      *
      * @return RoomCode
      */
-    public RoomDto createRoom(){
+    public RoomDto.RoomCode createRoom(){
         String code = RandomStringUtils.random(RANDOM_LEN, USE_LETTERS, USE_NUMBERS);
-        return new RoomDto(code);
+        return new RoomDto.RoomCode(code);
+    }
+
+    public boolean enterRoom(RoomDto.RoomCode roomDto) {
+        return true;
+    }
+
+    public void enterRoom(RoomDto.RequestEnter enterDto) {
+        log.info("Enter Room: " + enterDto);
+        // TODO: Redis 조회 후 대기방 정보(대기인원, 준비 상태) 넘겨주기
+        kafkaTemplate.send(TOPIC, enterDto);
+    }
+
+    @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
+    public void announceRoom(RoomDto.RequestEnter enterDto) {
+        log.info("Announce Enter Room : " + enterDto);
+        template.convertAndSend("/topic/room/"+enterDto.getRoomCode(), enterDto);
     }
 }
