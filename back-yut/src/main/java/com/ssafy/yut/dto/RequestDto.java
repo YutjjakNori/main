3a81a6d2fa6bba45480296a7dbd260e3f2b6abfd
김정은 <jegarnet23@gmail.com>
2023-04-27 16:36:09 +0900

    Feat : Request 클래스 생성

---
+++ back-yut/src/main/java/com/ssafy/yut/dto/RequestDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.yut.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestDto {
+    private String userId;
+    private String roomCode;
+}
--- back-yut/src/main/java/com/ssafy/yut/dto/YutDto.java
+++ back-yut/src/main/java/com/ssafy/yut/dto/YutDto.java
@@ -12,17 +12,6 @@ import lombok.NoArgsConstructor;
  */
 public class YutDto {
     /**
-     * 윷 던질 때 요청
-     */
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Request{
-        public String userId;
-        public String roomCode;
-    }
-
-    /**
      * 윷 던진 후 응답
      */
     @Getter
@@ -30,7 +19,7 @@ public class YutDto {
     @AllArgsConstructor
     @Builder
     public static class Response{
-        public String userId;
-        public int result;
+        private String userId;
+        private int result;
     }
 }
