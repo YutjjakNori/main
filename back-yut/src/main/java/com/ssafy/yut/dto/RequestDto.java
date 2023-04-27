7cb7bdfad57165eee0afca1df311dd8b6dc990f1
김정은 <jegarnet23@gmail.com>
2023-04-27 16:36:47 +0900

    Modify : JavaDoc 작성

--- back-yut/src/main/java/com/ssafy/yut/dto/RequestDto.java
+++ back-yut/src/main/java/com/ssafy/yut/dto/RequestDto.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 요청 메시지 클래스
+ *
+ * @author 김정은
+ */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
