bb2100185cc80271e9ea8e221fd397b3aeef93c7
jun <ktf1406@naver.com>
2023-04-27 17:40:42 +0900

    Modify : Entity 작성

---
+++ back-yut/src/main/java/com/ssafy/yut/entity/Room.java
@@ -0,0 +1,19 @@
+package com.ssafy.yut.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Room {
+    private List<User> users;
+    private String ready;
+}
