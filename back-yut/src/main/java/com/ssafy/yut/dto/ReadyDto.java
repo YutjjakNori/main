d3d8acc4bd727c51faa29edbc871b672ff90d595
ktf1406 <ktf1406@naver.com>
2023-04-28 00:18:01 +0900

    Feat : 준비 Dto 생성

---
+++ back-yut/src/main/java/com/ssafy/yut/dto/ReadyDto.java
@@ -0,0 +1,40 @@
+package com.ssafy.yut.dto;
+
+import lombok.*;
+
+/**
+ * 준비 상태
+ *
+ * @author 이준
+ */
+public class ReadyDto {
+
+
+    /**
+     * 준비 요청
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ReadyRequest {
+        private String userId;
+        private boolean ready;
+        private String roomCode;
+    }
+
+    /**
+     * 준비 응답
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ReadyResponse {
+        private String userId;
+        private boolean ready;
+        private boolean start;
+    }
+}
