28042654b9f50ee671272b43e379f242bd088850
jaehe <jaehe0413@naver.com>
2023-04-28 13:17:10 +0900

    Fix : Modal Story 수정

--- front-yut/present/common/Modal/Modal.stories.tsx
+++ front-yut/present/common/Modal/Modal.stories.tsx
@@ -4,7 +4,6 @@ import type { Meta, StoryObj } from "@storybook/react";
 const meta: Meta = {
   title: "common modal", //storybook에서 보이는 실제 title
   component: Modal, //rendering 할 componenet
-  argTypes: { onClick: { action: "clicked" } },
 };
 
 export default meta;
@@ -12,6 +11,6 @@ type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
   args: {
-    show: false,
+    title: "TestTitile",
   },
 };
