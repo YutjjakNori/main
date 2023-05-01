fbb1e14a33965b997e11b29317ece2f34f835485
jun <ktf1406@naver.com>
2023-05-01 13:45:49 +0900

    Feat : 방 접속 유저 Entity

---
+++ back-yut/src/main/java/com/ssafy/yut/entity/GameUser.java
@@ -0,0 +1,25 @@
+package com.ssafy.yut.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GameUser {
+    private String userId;
+    private List<Integer> pieces;
+
+    @Override
+    public boolean equals(Object obj) {
+        GameUser user = (GameUser) obj;
+        return (user.getUserId().equals(userId) && user.getPieces().equals(pieces));
+    }
+}
