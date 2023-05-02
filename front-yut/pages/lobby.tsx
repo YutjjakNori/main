5bbd77134f7ec4e8751964b29d8a86cb1853ba06
김정은 <jegarnet23@gmail.com>
2023-05-02 17:11:55 +0900

    Design : Lobby 버튼 구현

--- front-yut/pages/lobby.tsx
+++ front-yut/pages/lobby.tsx
@@ -7,7 +7,12 @@ const lobby = () => {
   return (
     <>
       <StyledDiv>
-        <StyledContainer>윷짝놀이</StyledContainer>
+        <StyledLogo>윷짝놀이</StyledLogo>
+        <StyledContainer>
+          <RectButton mainColor="#825A92">방 만들기</RectButton>
+          <RectButton mainColor="#F07F7F">참여하기</RectButton>
+          <RectButton mainColor="#D8C6AD">게임설명</RectButton>
+        </StyledContainer>
       </StyledDiv>
     </>
   );
@@ -19,17 +24,50 @@ const StyledDiv = styled.div`
   height: 100vh;
 `;
 
-const StyledContainer = styled.div`
+const StyledLogo = styled.div`
   font-size: 40vmin;
   font-weight: bold;
   text-align: center;
-  padding: 15vh 0;
+  padding: 85px 0;
   background-image: url(${backgroundImage.src});
   background-repeat: no-repeat;
   background-size: 100% 100%;
   background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
+`;
+
+const StyledContainer = styled.div`
+  width: 70vw;
+  display: flex;
+  justify-content: space-between;
+  width: fit-contents;
+  margin: auto;
+`;
+
+const RectButton = styled.button<{ mainColor: string }>`
+  width: 250px;
+  padding: 10px;
+  font-size: 1.6em;
+  font-weight: bold;
+  background-color: #ffffff;
+  border: ${({ mainColor }) => mainColor ?? "black"} 3px solid;
+  border-radius: 30px;
+  color: ${({ mainColor }) => mainColor ?? "black"};
+  box-shadow: 0px 2px 3px grey;
+  position: relative;
+  cursor: pointer;
+  transition-duration: 0.1s;
+
+  &:active {
+    top: 3px;
+    box-shadow: none;
+  }
+
+  &:hover{
+    background-color: ${({ mainColor }) => mainColor ?? "black"};
+    color: #ffffff;
+  }
 `;
 
 export default lobby;
