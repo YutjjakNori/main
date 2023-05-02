3c0d5948330e3695f4ddc2f9a700b9fe9cc11857
김정은 <jegarnet23@gmail.com>
2023-05-02 16:52:18 +0900

    Design : 로비화면 로고 구현

--- front-yut/pages/lobby.tsx
+++ front-yut/pages/lobby.tsx
@@ -1,6 +1,35 @@
+import styled, { keyframes } from "styled-components";
+
+import backgroundImage from "../public/image/gyeungbokgung_sunset.jpg";
+
 //로비 페이지
 const lobby = () => {
-  return <></>;
+  return (
+    <>
+      <StyledDiv>
+        <StyledContainer>윷짝놀이</StyledContainer>
+      </StyledDiv>
+    </>
+  );
 };
+const StyledDiv = styled.div`
+  position: relative;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const StyledContainer = styled.div`
+  font-size: 40vmin;
+  font-weight: bold;
+  text-align: center;
+  padding: 15vh 0;
+  background-image: url(${backgroundImage.src});
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+`;
 
 export default lobby;
