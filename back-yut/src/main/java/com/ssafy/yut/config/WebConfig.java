82b8c36d68853c9555341d4579192065d670f19d
jun <ktf1406@naver.com>
2023-05-04 14:50:24 +0900

    Feat : 웹 설정 추가

---
+++ back-yut/src/main/java/com/ssafy/yut/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.ssafy.yut.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 웹 설정
+ *
+ * @author 이준
+ */
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Cors 설정
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "https://k8d109.p.ssafy.io",
+                        "https://www.yutjjak.com",
+                        "https://yutjjak.com",
+                        "http://localhost:3000",
+                        "http://192.168.100.184:3000",
+                        "http://192.168.100.130:3000",
+                        "http://192.168.100.89:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
