a02501bb4278bfd2b7252c270cde3e56b20823a4
jaehe <jaehe0413@naver.com>
2023-05-09 09:22:59 +0900

    Feat : 윷 말 아이콘 적용

--- front-yut/present/component/YutPieceCompo/PieceIcon.tsx
+++ front-yut/present/component/YutPieceCompo/PieceIcon.tsx
@@ -1,6 +1,8 @@
 import { YutPieceType } from "@/types/game/YutPieceTypes";
 import YakgwaIcon from "@/public/icon/yakgwa.svg";
+import FlowerRiceIcon from "@/public/icon/flowerrice.svg";
 import SongpyeonIcon from "@/public/icon/songpyeon.svg";
+import SsukRiceIcon from "@/public/icon/ssukrice.svg";
 
 // TODO : 각 icon 추가되면 수정하기
 const PieceIcon = (type: YutPieceType): any => {
@@ -8,11 +10,11 @@ const PieceIcon = (type: YutPieceType): any => {
     case "yakgwa":
       return <YakgwaIcon />;
     case "flowerRice":
-      return <YakgwaIcon />;
+      return <FlowerRiceIcon />;
     case "songpyeon":
       return <SongpyeonIcon />;
     case "ssukRice":
-      return <YakgwaIcon />;
+      return <SsukRiceIcon />;
   }
 };
 
