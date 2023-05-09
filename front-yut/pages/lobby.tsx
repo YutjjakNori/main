3d2868a8a85c6621d0d0fed85993e231b995815c
soshui <615ksh@gmail.com>
2023-05-09 14:52:24 +0900

    Hotfix : env변수 확인

--- front-yut/pages/lobby.tsx
+++ front-yut/pages/lobby.tsx
@@ -2,6 +2,12 @@ import LobbyLayout from "@/present/layout/lobby/LobbyLayout";
 
 //로비 페이지
 const lobby = () => {
+  console.log(
+    "process.env.NEXT_PUBLIC_SERVER_URL >>> ",
+    process.env.NEXT_PUBLIC_SERVER_URL
+  );
+  console.log("process.env.NEXT_PUBLIC_SERVER_URL >>> ");
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL);
   return (
     <>
       <LobbyLayout />
