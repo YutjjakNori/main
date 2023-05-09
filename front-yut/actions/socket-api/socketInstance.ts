8eaa3be1fe273ef2c2cd166adee8445d9a6a6d6d
soshui <615ksh@gmail.com>
2023-05-09 14:48:29 +0900

    Hotfix : socket connect

--- front-yut/actions/socket-api/socketInstance.ts
+++ front-yut/actions/socket-api/socketInstance.ts
@@ -19,8 +19,10 @@ let sessionId: string = "";
 //STOMP over SockJS 연결
 async function connect(): Promise<void> {
   await new Promise<void>((resolve, reject) => {
+    const url: string = `${process.env.NEXT_PUBLIC_SERVER_URL}/yut`;
+    console.log(url);
     // let socket = new SockJS(`${process.env.NEXT_PUBLIC_SERVER_URL}/yut`);
-    let socket = new SockJS(`https://k8d109.p.ssafy.io/yut`);
+    let socket = new SockJS(url);
 
     stompClient = Stomp.over(socket);
     stompClient.connect(
