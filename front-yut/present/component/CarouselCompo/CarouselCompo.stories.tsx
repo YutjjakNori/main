e6c3f27e5038eeefdf2e9b520fb7e9703c8ceb72
jaehe <jaehe0413@naver.com>
2023-05-18 00:39:10 +0900

    Modify : Carousel story

--- front-yut/present/component/CarouselCompo/CarouselCompo.stories.tsx
+++ front-yut/present/component/CarouselCompo/CarouselCompo.stories.tsx
@@ -12,9 +12,16 @@ type Story = StoryObj<typeof CarouselCompo>;
 export const Default: Story = {
   args: {
     contents: [
-      "https://cdn.pixabay.com/photo/2023/04/23/11/11/flowers-7945521_960_720.jpg",
-      "https://cdn.pixabay.com/photo/2023/05/02/17/33/blue-tit-7965696_960_720.jpg",
-      "https://cdn.pixabay.com/photo/2023/04/24/03/16/camping-7947056_960_720.jpg",
+      {
+        subTitle: "subTitle",
+        imgSrc:
+          "https://cdn.pixabay.com/photo/2023/04/23/11/11/flowers-7945521_960_720.jpg",
+      },
+      {
+        subTitle: "subTitle",
+        imgSrc:
+          "https://cdn.pixabay.com/photo/2023/05/02/17/33/blue-tit-7965696_960_720.jpg",
+      },
     ],
   },
 };
