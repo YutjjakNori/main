63912901cf85197e8ac89b6861c3072b94e5fe5d
이진우 <blu_mountain@naver.com>
2023-05-17 22:31:10 +0900

    Merge: FE/develop 버전으로 최신화

--- front-yut/pages/ready.tsx
+++ front-yut/pages/ready.tsx
@@ -1,22 +1,16 @@
 import useGameReset from "@/actions/hook/useGameReset";
 import ReadyLayout from "@/present/layout/ready/ReadyLayout";
-<<<<<<< HEAD
 import { useRouter } from "next/router";
-
-//로비 페이지
-const ready = () => {
-  const router = useRouter();
-=======
 import { useEffect } from "react";
 
 //로비 페이지
 const ready = () => {
+  const router = useRouter();
   const { resetGame } = useGameReset();
 
   useEffect(() => {
     resetGame();
   }, []);
->>>>>>> 84bf09bbca2cd1c5d2d52b286ce999b9dcbb4a26
 
   return (
     <>
